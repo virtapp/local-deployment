c0db82229cd3258e63192df0c4587feabccfa9ff
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-02-17 23:12:47 +0200

    update

--- install.sh
+++ install.sh
@@ -70,9 +70,8 @@ printf "\nYou should see 'dashboard' as a reponse below (if you do the ingress i
              
 kubectl apply -f ./${path_folder}/ingress-keyclock.yaml || true
 kubectl apply -f ./${path_folder}/ingress-argocd.yaml   || true
-sleep 5 && 
-kubectl get nodes -o wide && sleep 5
-terraform providers
+sleep 5 && kubectl get nodes -o wide && sleep 5
+terraform providers && kubectl get ing -A
 
              echo      "----- ............................. -----"
              echo           "---  CLUSTER IS READY  ---"
