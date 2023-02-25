2439f0f63195fe606f6d314d4231d1a2f8f978e6
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-02-25 23:19:56 +0200

    update

--- argocd.tf
+++ argocd.tf
@@ -5,7 +5,7 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "4.9.7"
+  version          = "4.9.11"
   create_namespace = true
   timeout = 300
 
